b8085d33ed2bfb7eb76be9400155ae4b6687fff6
Johnathan Snyder <jrsnyder@crimson.ua.edu>
2014-07-27 17:14:38 -0500

    Fix code formating issues

--- webbot.go
+++ webbot.go
@@ -3,12 +3,12 @@ package gobot
 
 import (
 	"io/ioutil"
+	"log"
 	"net/http"
-    "net/url"
-    "os"
-    "log"
+	"net/url"
+	"os"
 	"regexp"
-    "runtime"
+	"runtime"
 )
 
 var (
@@ -21,80 +21,79 @@ type VisitAction func(*http.Response)
 
 type ErrorAction func(*http.Request)
 
-type VisitDecision func(string)(bool)
-
+type VisitDecision func(string) bool
 
 type GoBot struct {
 	http.Client
-    OnVisit VisitAction
-    OnError ErrorAction
-    ShouldVisit VisitDecision
-    visited map[string]bool
+	OnVisit     VisitAction
+	OnError     ErrorAction
+	ShouldVisit VisitDecision
+	visited     map[string]bool
 }
 
 func NewGoBot() *GoBot {
-    bot := new(GoBot)
-    bot.OnVisit = defaultVisitAction
-    bot.OnError = defaultErrorAction
-    bot.ShouldVisit = defaultVisitDecision
-    bot.Jar = NewBotCookieJar()
-    bot.visited = make(map[string]bool)
-    return bot
+	bot := new(GoBot)
+	bot.OnVisit = defaultVisitAction
+	bot.OnError = defaultErrorAction
+	bot.ShouldVisit = defaultVisitDecision
+	bot.Jar = NewBotCookieJar()
+	bot.visited = make(map[string]bool)
+	return bot
 }
 
 func (bot *GoBot) StartCrawl(seed string) {
-    resp, err := bot.Get(seed)
-    ncpu := runtime.NumCPU()
-    runtime.GOMAXPROCS(ncpu)
-    if err != nil {
-        log.Printf("StartCrawl: ERROR\n")
-    }
-    urls := ExtractLinks(resp)
-    for i, u := range urls {
-        if i == (ncpu - 1) {
-            break
-        }
-        go bot.Crawl(u)
-    }
-    bot.Crawl(seed)
+	resp, err := bot.Get(seed)
+	ncpu := runtime.NumCPU()
+	runtime.GOMAXPROCS(ncpu)
+	if err != nil {
+		log.Printf("StartCrawl: ERROR\n")
+	}
+	urls := ExtractLinks(resp)
+	for i, u := range urls {
+		if i == (ncpu - 1) {
+			break
+		}
+		go bot.Crawl(u)
+	}
+	bot.Crawl(seed)
 }
 
 func (bot *GoBot) Crawl(seed string) {
-    queue := make([]string, 0)
-    currUrl := seed
+	queue := make([]string, 0)
+	currUrl := seed
 
-    for {
-        resp, err := bot.Get(currUrl)
-        for err != nil {
-            go bot.OnError(resp.Request)
-            if len(queue) > 0 {
-                currUrl = queue[0]
-                queue = queue[1:]
-                resp, err = bot.Get(currUrl)
-            } else {
-                os.Exit(1) // TODO: Should find a better way to exit.
-            }
-        }
+	for {
+		resp, err := bot.Get(currUrl)
+		for err != nil {
+			go bot.OnError(resp.Request)
+			if len(queue) > 0 {
+				currUrl = queue[0]
+				queue = queue[1:]
+				resp, err = bot.Get(currUrl)
+			} else {
+				os.Exit(1) // TODO: Should find a better way to exit.
+			}
+		}
 
-        go bot.OnVisit(resp)
+		go bot.OnVisit(resp)
 
-        urls := ExtractLinks(resp)
-        for _, url := range urls {
-            _, present := bot.visited[url]
-            if !present {
-                queue = append(queue, url)
-                bot.visited[url] = true
-            }
-        }
+		urls := ExtractLinks(resp)
+		for _, url := range urls {
+			_, present := bot.visited[url]
+			if !present {
+				queue = append(queue, url)
+				bot.visited[url] = true
+			}
+		}
 
-        if len(queue) > 0 {
-            currUrl = queue[0]
-            queue = queue[1:]
-        } else {
-            break
-        }
+		if len(queue) > 0 {
+			currUrl = queue[0]
+			queue = queue[1:]
+		} else {
+			break
+		}
 
-    }
+	}
 }
 
 // Extracts all the links from the body of an http response.
@@ -123,31 +122,31 @@ func ResponseBodyToString(resp *http.Response) string {
 }
 
 func defaultVisitAction(resp *http.Response) {
-    request := resp.Request
-    log.Printf("%s\n", request.URL.String())
+	request := resp.Request
+	log.Printf("%s\n", request.URL.String())
 }
 
 func defaultErrorAction(resp *http.Request) {
 }
 
 func defaultVisitDecision(url string) bool {
-    return true
+	return true
 }
 
 type BotCookieJar struct {
-    cookies map[string][]*http.Cookie
+	cookies map[string][]*http.Cookie
 }
 
 func NewBotCookieJar() *BotCookieJar {
-    jar := new(BotCookieJar)
-    jar.cookies = make(map[string][]*http.Cookie)
-    return jar
+	jar := new(BotCookieJar)
+	jar.cookies = make(map[string][]*http.Cookie)
+	return jar
 }
 
 func (jar *BotCookieJar) SetCookies(u *url.URL, cookies []*http.Cookie) {
-    jar.cookies[u.Host] = cookies
+	jar.cookies[u.Host] = cookies
 }
 
 func (jar *BotCookieJar) Cookies(u *url.URL) []*http.Cookie {
-    return jar.cookies[u.Host]
+	return jar.cookies[u.Host]
 }
